b4e9acc3023a43abc04bbd93b6d9362309e82e2b
최호찬 <starhochan70@gmail.com>
2026-07-09 16:01:05 +0900

    firebase.js에서 불필요한 주석 제거 및 Firestore 초기화 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
@@ -18,8 +12,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEARSUREMENTID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// export const firebase = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
+export const db = getFirestore(app);
